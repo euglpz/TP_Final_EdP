3f26e379d9c840a9d0321c09627ed80be38d2068
eugenio <91112771+euglpz@users.noreply.github.com>
2022-08-05 12:49:08 -0300

    Update findNames.sh

--- findNames.sh
+++ findNames.sh
@@ -7,6 +7,10 @@
 FILE=$1		
 NOMBRE_PROPIO="^[A-Z]{1}[a-z]+$"	#Regex para formato de nombre propio: Una mayúscula + Al menos una minúscula
 
+OPEN_FILE=$(cat $FILE)
+
+[[ $OPEN_FILE =~ "" ]] && echo "No hay palabras en el texto" && exit 1
+
 #Elimino caracteres indeseados e incorporo salto de linea.
 ARREGLO=$(cat $FILE | tr -d '.,?!:' | tr ' ' '\n')
 
