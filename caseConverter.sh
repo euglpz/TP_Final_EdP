48e7f621ce9f86e5601ff26f76530876b5a5de49
eugenio <91112771+euglpz@users.noreply.github.com>
2022-08-05 12:48:42 -0300

    Update caseConverter.sh

--- caseConverter.sh
+++ caseConverter.sh
@@ -8,6 +8,10 @@ FILE=$1
 FILE_CONT=$(cat $FILE)		#Guardo el contenido del archivo
 FILE_INV="${FILE_CONT~~}"	#Invierto el case de los caracteres
 
+OPEN_FILE=$(cat $FILE)
+
+[[ $OPEN_FILE =~ "" ]] && echo "No hay palabras en el texto" && exit 1
+
 echo "Texto original:"
 echo "---------------"
 echo "$FILE_CONT"
